2bc7cf86effc73204b494ed6bc1294b2d4e78028
mubeenwd <mubeenavailable@gmail.com>
2014-06-13 02:51:33 +0530

    Initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,14 @@ $('[data-toggle=offcanvas]').click(function() {
 $('.btn-toggle').click(function() {
   $(this).find('.btn').toggleClass('active').toggleClass('btn-default').toggleClass('btn-primary');
 });
+
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.href);
+    if (results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
